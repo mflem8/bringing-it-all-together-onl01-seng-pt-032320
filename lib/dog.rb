08511e19ab47c59b295204ad0a94f0e8aa8fbcd8
mflem8 <matthewfleming8@gmail.com>
2020-05-29 14:21:25 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,6 +65,31 @@ class Dog
     end.first
   end
 
+  def self.find_or_create_by(name:, breed:)
+   sql = <<-SQL
+         SELECT *
+         FROM dogs
+         WHERE name = ?
+         AND breed = ?
+         LIMIT 1
+       SQL
+
+   dog = DB[:conn].execute(sql,name,breed)
+
+   if !dog.empty?
+     dog_data = dog[0]
+     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+   else
+     dog = self.create(name: name, breed: breed)
+   end
+   dog
+ end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
   def save
     if self.id
       self.update
